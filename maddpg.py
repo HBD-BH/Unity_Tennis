27a6a605341b7fac7d5ab9572ddb3ff9f3b4903f
Bernhard Häfner <bernhardhaefner@hotmail.com>
2020-04-27 10:31:30 +0200

    style: Delete legacy functions from maddpg

--- maddpg.py
+++ maddpg.py
@@ -24,16 +24,6 @@ class MADDPG_Agent():
         self.tstep = 0
 
 
-    #def get_actors(self):
-        #"""Get actual actors of all the agents in the MADDPG object"""
-        #actors = [ddpg_agent.actor_local for ddpg_agent in self.ddpg_agents]
-        #return actors
-
-    #def get_target_actors(self):
-        #"""Get target actors of all the agents in the MADDPG object"""
-        #target_actors = [ddpg_agent.actor_target for ddpg_agent in self.ddpg_agents]
-        #return target_actors
-
     def act(self, states_all_agents, noise=0.0):
         """Get actions from all agents in the MADDPG object.
 
@@ -46,30 +36,15 @@ class MADDPG_Agent():
         # Transform states and next_states into row vectors
         states_all_agents = np.reshape(states_all_agents, (1,self.num_agents*self.state_size))
 
-        # Calculation of actions
+        # Calculate actions
         actions = self.ddpg_agents[0].act(states_all_agents, noise)
         for agent in self.ddpg_agents[1:]:
             action = agent.act(states_all_agents,noise)
             actions = np.concatenate((actions, action), axis=0)
         actions = np.reshape(actions, (1, self.num_agents*self.action_size))
 
-        #actions = [agent.act(states_all_agents, noise) for agent in self.ddpg_agents]
-        # Transform from list to (1,numagents*action_size) ndarray
-        #actions = np.resize(np.asarray(actions, dtype=np.float32), (1,self.num_agents*self.action_size))
         return actions
 
-    '''
-    def act_targets(self, states_all_agents, noise=0.0):
-        """Get actions by target networks from all agents in the MADDPG object.
-Params
-        ======
-            states_all_agents (array-like): the states for each agent
-            noise: the noise to apply
-        """
-
-        actions = [agent.act_targets(state, noise) for agent, state in zip(self.ddpg_agents, states_all_agents)]
-        return actions
-    '''
 
     def step(self, states, actions, rewards, next_states, done,beta=0):
         """ Save experience in replay memory, and learn new target weights
